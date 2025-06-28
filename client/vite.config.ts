1216cb91b6b6cf058b2d33437688bf877a4a2168
Mychael Magnuson <mychaelmagnuson@gmail.com>
2025-06-27 20:39:41 -0700

    Initial project structure: frontend + backend + DB setup

---
+++ client/vite.config.ts
@@ -0,0 +1,8 @@
+import { defaultClientMainFields, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Territory.io backend is running 🚀");
+});
+
+app.listen(4000, () => {
+  console.log("Server running on http://localhost:4000");
+});
