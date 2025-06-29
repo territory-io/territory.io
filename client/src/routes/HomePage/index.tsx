b2c90b969e3d1aa5357d89e6be8a0d7a6d812e77
Mychael Magnuson <mychaelmagnuson@gmail.com>
2025-06-28 19:55:58 -0700

    Creating route for the Home Page, needs to be edited; CREATE IF ELSE STRUCTURE FOR USER LOG IN STATE ONCE ESTABLISHED

---
+++ client/src/routes/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of the application.</p>
+      {/* You can add more components or content here */}
+    </div>
+  );
+};
+
+export default Home;
