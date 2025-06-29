b6936db82e8b52d54f56207cf4913dfa117eb838
Mychael Magnuson <mychaelmagnuson@gmail.com>
2025-06-29 13:19:09 -0700

    Building file structure for upcoming backend progress. seed data will be created in order to use for testing auth states. and functionality will begin to be developed to help with login form structure and ensure data can be added to the db through registration form

---
+++ server/db/client.ts
@@ -0,0 +1,15 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+const connectionString =
+  process.env.DATABASE_URL || "https://localhost:5432/server";
+
+const client = new Client({
+  connectionString,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+module.exports = client;
