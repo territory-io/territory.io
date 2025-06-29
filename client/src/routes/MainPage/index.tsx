9bb6cf606a67efab8a8c275561cf0920d9149faa
Mychael Magnuson <mychaelmagnuson@gmail.com>
2025-06-29 14:19:44 -0700

    Restructuring the MainPage index.tsx file to do an auth check for logged in and grouped status; only users in a group and logged in will be able to access the map

--- client/src/routes/MainPage/index.tsx
+++ client/src/routes/MainPage/index.tsx
@@ -1,9 +1,12 @@
 import Map from "./components/Map";
 // import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import React from "react";
+import { isLoggedIn } from "../../utils/isLoggedIn";
+import { isGrouped } from "../../utils/isGrouped"; // Assuming this utility checks if the user is part of a group/team
+import { Navigate } from "react-router-dom";
 
 const MainPage: React.FC = () => {
-  return (
+  return isLoggedIn() && isGrouped() ? (
     <div
       style={{
         display: "flex",
@@ -15,6 +18,8 @@ const MainPage: React.FC = () => {
     >
       <Map />
     </div>
+  ) : (
+    <Navigate to="/login" />
   );
 };
 
