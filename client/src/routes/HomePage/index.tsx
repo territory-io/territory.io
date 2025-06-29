e0d86dab4788376b079eca189657cb9056d63878
Mychael Magnuson <mychaelmagnuson@gmail.com>
2025-06-29 10:03:46 -0700

    Changing Home Page to call the isLoggedIn function for auth state before displaying page

--- client/src/routes/HomePage/index.tsx
+++ client/src/routes/HomePage/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { isLoggedIn } from "../../utils/isLoggedIn";
+import { Navigate } from "react-router-dom";
 
 const Home: React.FC = () => {
-  return (
+  // if user is not logged in, redirect to login page
+  // if user is logged in, display the home page content
+  return isLoggedIn() ? (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page of the application.</p>
       {/* You can add more components or content here */}
     </div>
+  ) : (
+    <Navigate to="/login" />
   );
 };
 
